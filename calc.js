46d9f0bbb846dba2eb8362dff761e1d04f0120a5
mroseweir <roseweirmichael@gmail.com>
2021-06-14 15:16:13 -0500

    adds calculator funtion

--- calc.js
+++ calc.js
@@ -13,6 +13,28 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	
+
+	console.log('num1', num1);
+	console.log('mathSymbol', mathSymbol);
+	console.log('num2', num2);
+
+	if (mathSymbol === ("+")){
+		console.log('Solution' , num1 + num2);
+	}
+	if (mathSymbol === ("-")){
+		console.log('Solution' , num1 - num2)
+	}
+	if (mathSymbol === ("/")){
+		console.log('Solution' , num1 / num2)
+	}
+	if (mathSymbol === ("*")){
+		console.log('Solution' , num1 * num2)
+	}
+	if (mathSymbol === ("sqrt")){
+		console.log('Solution' , Math.sqrt(num1))
+	}
+
 
 
 	// This line closes the connection to the command line interface.
